73a018d8fc96c6632a4c6d9540366b597a6ffdd5
Athil khan <athilkhan2005@gmail.com>
2025-11-15 17:03:31 +0530

    V1

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
 from flask import Flask, render_template, request
+from keras.models import load_model
+import numpy as np
+
+model = load_model('fish.h5')
+model_d = load_model('disaster.h5')
 
 app = Flask(__name__)
 
@@ -9,12 +14,20 @@ def index():
 @app.route("/predict", methods=["GET", "POST"])
 def predict():
     if request.method == "POST":
-        tdrop = float(request.form.get("tdrop", 28))
-        rainocn = float(request.form.get("rainocn", 0.3))
+        month = float(request.form.get("month", 1))
+        press = float(request.form.get("pressure", 1000))
+        temp = float(request.form.get("temp", 22))
+        tdrop = float(request.form.get("tempd", 2))
+        rain = float(request.form.get("rain", 0.3))
         tskinice = float(request.form.get("tskinice", 25))
+
+        fish = np.array([[temp, tdrop, rain, press, month]])
+        disaster = np.array([[temp, tdrop, tskinice, rain]])
         
-        fishing_prob = 0.5 if tdrop >= 28 and rainocn <= 0.5 else 0.2
-        disaster_prob = 0.5 if tskinice >= 29 and rainocn >= 0.6 else 0.1
+        fishing_prob = model.predict(fish)
+        disaster_prob = model_d.predict(disaster)
+
+        print(fishing_prob, disaster_prob)
         
         return render_template("predict.html", fishing_prob=fishing_prob, disaster_prob=disaster_prob)
     return render_template("predict.html")
@@ -25,3 +38,4 @@ def dashboard():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
