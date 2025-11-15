f86f377f138781f39acf4557c6f6f7e47038a24d
Athil khan <athilkhan2005@gmail.com>
2025-11-15 12:45:07 +0530

    V3

--- models/ai_integration.py
+++
@@ -1,6 +0,0 @@
-from .train_and_load import ensure_models_exist
-from .fishing_model import predict_fishing
-from .disaster_model import predict_disaster
-
-def build_all_if_missing():
-    ensure_models_exist()
--- routes/api.py
+++
@@ -1,1 +0,0 @@
-# This file is no longer needed as predictions are handled directly in app.py
